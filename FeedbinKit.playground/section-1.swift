f7889cbbe3efb7b19d609061b017753de83bdce0
Bill Williams <bill@flpatriot.com>
2014-11-11 16:33:00 -0800

    Fix for error when JSON string is empty.

--- FeedbinKit.playground/section-1.swift
+++ FeedbinKit.playground/section-1.swift
@@ -6,12 +6,16 @@ import ObjectMapper
 
 
 let savedSearchesJSON = "[{\"id\": 1, \"name\": \"JavaScript\", \"query\": \"javascript is:unread\" }]"
+let searches: [SavedSearch]? = Mapper().map(savedSearchesJSON, to: SavedSearch.self)
 
-let searches: [SavedSearch] = Mapper().map(savedSearchesJSON, to: SavedSearch.self)
-for search in searches {
-    if let name = search.name {
-        println(name)
-    } else {
-        println("Unnamed")
+if let searches = searches {
+    println("# Saved searches")
+    for search in searches {
+        if let name = search.name {
+            println("- \(name)")
+        } else {
+            println("- Unnamed")
+        }
     }
+    print("\n\n")
 }
